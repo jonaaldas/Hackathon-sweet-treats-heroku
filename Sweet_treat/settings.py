2ac1d6bbd21fbb817ab43971df6875aff3b2cc73
Steven Palfreyman <ironmonkus@gmail.com>
2022-02-08 11:11:45 -0500

    Adjusting inventory

--- Sweet_treat/settings.py
+++ Sweet_treat/settings.py
@@ -141,3 +141,12 @@ mimetypes.add_type("text/css", ".css", True)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sweettreatsalerts@gmail.com'
+EMAIL_HOST_PASSWORD = 'SweetTreats2022'
--- sweet_treat_app/views.py
+++ sweet_treat_app/views.py
@@ -4,6 +4,10 @@
 # # import bcrypt
 # import re
 # EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
@@ -17,3 +21,26 @@ def about(request):
 
 def inventory(request):
     return render(request,'sweet_treat_app/inventory.html')
+
+
+# EMAIL
+
+def signup(request):
+    if request.method=="POST":
+        username =request.POST["username"]
+        password =request.POST["password"]
+        email = request.POST["email"]
+
+        user = User.objects.create_user(
+                username = username,
+                password = password,
+                email = email
+             )
+        login (request,user)
+        subject = 'Welcome to Sweet Treats'
+        message = f'Hi {user.username}, thank you for signing up to the Sweet Treats mailing list!'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [user.email, ]
+        send_mail( subject, message, email_from, recipient_list )
+        return redirect("/dashboard/")
+    return render(request, "signup.html")
